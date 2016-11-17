bd68c6120c6366b98570e54ce34073f8fee773c5
code2Vici <naborp@cpp.edu>
2016-11-16 19:37:09 -0800

    2nd Commit

--- app/src/main/java/com/example/naborp/focusgame/MainGame.java
+++ app/src/main/java/com/example/naborp/focusgame/MainGame.java
@@ -1,5 +1,5 @@
 package com.example.naborp.focusgame;
-/*Main Game class where the cards are diplayed hi mi name is ubaldo*/
+/*Main Game class where the cards are diplayed hi mi name is ubalddknco*/
 
 import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;

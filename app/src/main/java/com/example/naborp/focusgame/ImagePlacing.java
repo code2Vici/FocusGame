499a4126662f35819ac8a351408deacbd5cc064e
SimplyValdo <simplyvaldo@gmail.com>
2016-11-22 18:04:14 -0800

    Valdo's Commit

--- app/src/main/java/com/example/naborp/focusgame/ImagePlacing.java
+++ app/src/main/java/com/example/naborp/focusgame/ImagePlacing.java
@@ -84,7 +84,7 @@ public class ImagePlacing extends BaseAdapter {
 
         if (convertView == null) {
             imageView = new ImageView(mContext);
-            imageView.setLayoutParams(new GridView.LayoutParams(180,180));
+            imageView.setLayoutParams(new GridView.LayoutParams(200,200));
             imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
         }
         else {
